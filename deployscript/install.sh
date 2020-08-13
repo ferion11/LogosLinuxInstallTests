c97e612d2bc348b2615a937ed51b4f0029689666
DanielDevBR <dados.linux@gmail.com>
2020-08-13 16:18:56 -0300

    removing -q to see more detail

--- deployscript/install.sh
+++ deployscript/install.sh
@@ -12,4 +12,4 @@ sudo add-apt-repository -y ppa:cybermax-dexter/sdl2-backport >/dev/null || die "
 wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo add-apt-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ ${CHROOT_DISTRO} main" >/dev/null
 sudo apt-get -q -y update >/dev/null
-sudo apt-get -q install -y procps mpg123 x264 ffmpeg xvfb xdotool x11-apps zenity winbind cabextract winetricks fuseiso binutils policykit-1 xdg-utils unzip unrar p7zip-full wget aria2 tor git sudo tar gzip xz-utils bzip2 gawk sed winehq-staging=5.11~bionic wine-staging=5.11~bionic wine-staging-amd64=5.11~bionic wine-staging-i386=5.11~bionic || die "* main apt fail!"
+sudo apt-get install -y procps mpg123 x264 ffmpeg xvfb xdotool x11-apps zenity winbind cabextract winetricks fuseiso binutils policykit-1 xdg-utils unzip unrar p7zip-full wget aria2 tor git sudo tar gzip xz-utils bzip2 gawk sed winehq-staging=5.11~bionic wine-staging=5.11~bionic wine-staging-amd64=5.11~bionic wine-staging-i386=5.11~bionic || die "* main apt fail!"
