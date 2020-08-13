642b88c0a697966b5cde214fd7e5789f117c3fa5
DanielDevBR <dados.linux@gmail.com>
2020-08-13 16:40:46 -0300

    added --add-architecture i386

--- deployscript/install.sh
+++ deployscript/install.sh
@@ -6,6 +6,7 @@ export CHROOT_DISTRO="bionic"
 export CHROOT_MIRROR="http://archive.ubuntu.com/ubuntu/"
 
 # add deps for wine:
+sudo dpkg --add-architecture i386
 sudo add-apt-repository -y ppa:cybermax-dexter/sdl2-backport || die "* add-apt-repository fail!"
 
 # updating wine https://wiki.winehq.org/Ubuntu:
@@ -13,5 +14,4 @@ wget https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add <./winehq.key
 sudo add-apt-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ ${CHROOT_DISTRO} main"
 sudo apt-get -q -y update
-sudo apt-get install -y procps mpg123 x264 ffmpeg xvfb xdotool x11-apps zenity winbind cabextract winetricks fuseiso binutils policykit-1 xdg-utils unzip unrar p7zip-full wget aria2 tor git sudo tar gzip xz-utils bzip2 gawk sed winehq-staging wine-staging wine-staging-amd64 wine-staging-i386 || die "* main apt fail!"
-#sudo apt-get install -y winehq-staging=5.11~bionic wine-staging=5.11~bionic wine-staging-amd64=5.11~bionic wine-staging-i386=5.11~bionic || die "* wine apt fail!"
+sudo apt-get install -y procps mpg123 x264 ffmpeg xvfb xdotool x11-apps zenity winbind cabextract winetricks fuseiso binutils policykit-1 xdg-utils unzip unrar p7zip-full wget aria2 tor git sudo tar gzip xz-utils bzip2 gawk sed winehq-staging=5.11~bionic wine-staging=5.11~bionic wine-staging-amd64=5.11~bionic wine-staging-i386=5.11~bionic || die "* main apt fail!"
