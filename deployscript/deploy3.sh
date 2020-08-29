eabfd088d777d341a93585830fcdd41dbef14916
DanielDevBR <dados.linux@gmail.com>
2020-08-28 21:28:05 -0300

    clean some packages, added some time to winebottle update step1

--- deployscript/deploy3.sh
+++ deployscript/deploy3.sh
@@ -198,6 +198,7 @@ echo "* wine mono cancel:"
 close_wine_mono_init_windows
 echo "* wine gecko cancel:"
 close_wine_gecko_init_windows
+sleep 7
 echo "* wine gecko cancel (part2):"
 close_wine_gecko_init_windows
 
