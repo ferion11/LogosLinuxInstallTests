6b46b3c2b80a710a8e8fe005a1f58b2bdb1c525d
DanielDevBR <dados.linux@gmail.com>
2020-08-31 13:14:29 -0300

    added op5 test from v2.12-rc0, and custom install.sh for each option, refactory too

---
+++ deployscript/deploy5.sh
@@ -0,0 +1,296 @@
+#!/bin/bash
+#=================================================
+die() { echo >&2 "$*"; exit 1; };
+#=================================================
+echo "------- Running: -------"
+head -3 ./install_AppImageWine_and_Logos.sh
+echo "---------------------"
+
+if [ -z "$WORKDIR" ]; then export WORKDIR="$(mktemp -d)" ; fi
+if [ -z "$INSTALLDIR" ]; then export INSTALLDIR="$HOME/LogosBible_Linux_P_5" ; fi
+
+echo "******* Option 5 *******"
+export DISPLAY=:94.0
+
+echo "======= DEBUG: Starting xvfb ======="
+Xvfb $DISPLAY -screen 0 1024x768x24 &
+Xvfb_PID=$!
+sleep 7
+echo "* Using DISPLAY: $DISPLAY"
+
+#=================================================
+PRINT_NUM=1
+printscreen() {
+	xwd -display $DISPLAY -root -silent | convert xwd:- png:./screenshots_5/img_${PRINT_NUM}.png
+	PRINT_NUM=$((PRINT_NUM+1))
+}
+#=================================================
+
+close_question_1_yes_5_windows() {
+	while ! WID=$(xdotool search --name "Question: Install Logos Bible"); do
+		sleep "1"
+	done
+	echo "* Sending installer keystrokes..."
+	xdotool key --delay 500 Tab
+	sleep "0.5"
+	xdotool key --delay 500 Tab
+	sleep "0.5"
+	xdotool key --delay 500 Tab
+	sleep "0.5"
+	xdotool key --delay 500 Down
+	sleep "0.5"
+	xdotool key --delay 500 Down
+	sleep "0.5"
+	xdotool key --delay 500 Down
+	sleep "0.5"
+	xdotool key --delay 500 Down
+	sleep "0.5"
+	xdotool key --delay 500 space
+	sleep "0.5"
+	xdotool key --delay 500 Tab
+	sleep "0.5"
+	xdotool key --delay 500 Tab
+	sleep "0.5"
+	printscreen
+	xdotool key --delay 500 space
+	sleep "0.5"
+}
+
+close_question_yes_windows() {
+	while ! WID=$(xdotool search --name "Question:*"); do
+		sleep "1"
+	done
+	printscreen
+	echo "* Sending installer keystrokes..."
+	xdotool key --delay 500 space
+	sleep "0.5"
+}
+
+close_question_no_windows() {
+	while ! WID=$(xdotool search --name "Question:*"); do
+		sleep "1"
+	done
+	echo "* Sending installer keystrokes..."
+	xdotool key --delay 500 Tab
+	sleep "0.5"
+	xdotool key --delay 500 Tab
+	sleep "0.5"
+	printscreen
+	xdotool key --delay 500 space
+	sleep "0.5"
+}
+
+close_wine_mono_init_windows() {
+	while ! WID=$(xdotool search --name "Wine Mono Installer"); do
+		sleep "1"
+	done
+	echo "Sending installer keystrokes..."
+	xdotool key --window $WID --delay 500 Tab
+	sleep "0.5"
+	printscreen
+	xdotool key --window $WID --delay 500 space
+	sleep "0.5"
+}
+
+close_wine_gecko_init_windows() {
+	while ! WID=$(xdotool search --name "Wine Gecko Installer"); do
+		sleep "1"
+	done
+	echo "Sending installer keystrokes..."
+	xdotool key --window $WID --delay 500 Tab
+	sleep "0.5"
+	printscreen
+	xdotool key --window $WID --delay 500 space
+	sleep "0.5"
+}
+
+wait_window_and_print(){
+	echo "* start waiting for $@ ..."
+	while ! WID=$(xdotool search --name "$@"); do
+		sleep "0.2"
+	done
+	echo "... found \"${*}\"! And print:"
+	printscreen
+}
+
+logos_install_window(){
+	while ! WID=$(xdotool search --name "Logos Bible Software Setup"); do
+		sleep "1"
+	done
+	printscreen
+	sleep 7
+	printscreen
+	echo "* Sending installer keystrokes..."
+	xdotool key --delay 500 space
+	sleep 3
+	printscreen
+	xdotool key --delay 500 space
+	sleep "0.5"
+	xdotool key --delay 500 Tab
+	sleep "0.5"
+	xdotool key --delay 500 Tab
+	sleep "0.5"
+	xdotool key --delay 500 Tab
+	sleep "0.5"
+	printscreen
+	xdotool key --delay 500 space
+	sleep 3
+	printscreen
+	xdotool key --delay 500 space
+	sleep 3
+	printscreen
+	xdotool key --delay 500 space
+	echo "... waiting 30s for the last screen ..."
+	printscreen
+	sleep 2
+	printscreen
+	sleep 28
+	echo "... end of 30s, screenshot and space key:"
+	printscreen
+	xdotool key --delay 500 space
+}
+
+echo "* Starting the video record:"
+ffmpeg -loglevel quiet -f x11grab -video_size 1024x768 -i $DISPLAY -codec:v libx264 -r 12 video5.mp4 &
+FFMPEG_PID=${!}
+finish_the_script_at_end() {
+	echo "------- Ending for DEBUG -------"
+	# some more info:
+	ps ux | grep wine
+	printscreen
+
+	kill -SIGTERM "${FFMPEG_PID}"
+	sleep 2
+	kill -SIGTERM "${Xvfb_PID}"
+	sleep 2
+	tar cvzf screenshots_5.tar.gz screenshots_5
+
+	exit 0
+}
+
+#-------------------------------------------------
+wait_for_wine_process() {
+	echo "* wineserver -w"
+	WINEARCH=win64 WINEPREFIX="${INSTALLDIR}/data/wine64_bottle" wineserver -w
+}
+killall_for_wine_process() {
+	echo "* wineserver -k"
+	WINEARCH=win64 WINEPREFIX="${INSTALLDIR}/data/wine64_bottle" wineserver -k
+}
+#-------------------------------------------------
+#===========================================================================================
+mkdir screenshots_5
+
+chmod +x ./install_AppImageWine_and_Logos.sh
+
+echo "* Starting install_AppImageWine_and_Logos.sh"
+./install_AppImageWine_and_Logos.sh &
+INSTALL_SCRIPT_PID=${!}
+#--------
+
+
+# Starting Steps here:
+echo "* Question: using the AppImage installation (option 5):"
+close_question_1_yes_5_windows
+
+sleep "0.5"
+echo "* Downloading fake AppImage:"
+printscreen
+sleep 2
+echo "* Downloading wine 5.11 to install up to dotnet48:"
+printscreen
+
+echo "extracting wine 5.11"
+wait_window_and_print "Extracting..."
+
+
+echo "* Question: wine bottle:"
+close_question_yes_windows
+
+
+echo "* Waiting to initialize wine..."
+# need another gecko step for 64bit
+echo "* wine mono cancel:"
+close_wine_mono_init_windows
+echo "* wine gecko cancel:"
+close_wine_gecko_init_windows
+sleep 7
+echo "* wine gecko cancel (part2):"
+close_wine_gecko_init_windows
+wait_for_wine_process
+
+echo "* ls -la on INSTALLDIR/data"
+ls -la "${INSTALLDIR}/data"
+
+
+echo "* Question: winetricks:"
+close_question_yes_windows
+
+echo "* waiting Winetricks corefonts"
+wait_window_and_print "Winetricks corefonts"
+
+echo "* waiting Winetricks fontsmooth"
+wait_window_and_print "Winetricks fontsmooth"
+
+echo "* waiting Winetricks dotnet48"
+wait_window_and_print "Winetricks dotnet48"
+
+echo "* waiting Winetricks dotnet48 end..."
+echo "find sub-process winetricks:"
+WINETRICKS_PID="$(pgrep -P "${INSTALL_SCRIPT_PID}" winetricks)"
+echo "wait for linux process WINETRICKS_PID: ${WINETRICKS_PID}"
+tail --pid="${WINETRICKS_PID}" -f /dev/null
+
+#-------
+sleep 1
+printscreen
+sleep 1
+echo "ps ux | grep wine"
+echo "$(ps ux | grep wine)"
+echo "* Changing to native wine-staging 64bits installation..."
+sleep 1
+printscreen
+
+echo "* Waiting to initialize last wine..."
+echo "* wine gecko cancel:"
+close_wine_gecko_init_windows
+#-------
+wait_for_wine_process
+
+
+echo "* Question: download and install Logos"
+close_question_yes_windows
+
+echo "* Downloading Logos:"
+sleep 1
+printscreen
+
+echo "* Logos install window:"
+logos_install_window
+wait_for_wine_process
+
+
+echo "* Question: run Logos.sh"
+close_question_yes_windows
+
+echo "... waiting 21s to Logos start:"
+sleep 21
+printscreen
+
+#echo "find sub-process Logos.sh:"
+#LOGOS_SH_PID="$(pgrep -P "${INSTALL_SCRIPT_PID}" Logos.sh)"
+#echo "sending signal 15 to Logos.sh with PID: ${LOGOS_SH_PID}"
+#kill -SIGTERM "${LOGOS_SH_PID}"
+#sleep 2
+killall_for_wine_process
+#---------------
+
+kill -SIGTERM "${FFMPEG_PID}"
+sleep 2
+# kill Xvfb whenever you feel like it
+echo "* Stopping the Xvfb ..."
+kill -SIGTERM "${Xvfb_PID}"
+sleep 2
+#---------------
+
+tar cvzf screenshots_5.tar.gz screenshots_5
--- deployscript/install.sh
+++ deployscript/install.sh
@@ -6,13 +6,72 @@ export WINEHQ_STAGING_VERSION="5.11"
 export UBUNTU_DISTRO="bionic"
 export UBUNTU_MIRROR="http://archive.ubuntu.com/ubuntu/"
 
-# add deps for wine:
-sudo dpkg --add-architecture i386 >/dev/null
-sudo add-apt-repository -y ppa:cybermax-dexter/sdl2-backport >/dev/null || die "* add-apt-repository fail!"
+install_minimal_monitor() {
+	sudo apt-get -q -y install procps psmisc mpg123 x264 ffmpeg xvfb xdotool zenity winbind cabextract gawk sed >/dev/null || die "* main apt fail!"
+}
 
-# updating wine https://wiki.winehq.org/Ubuntu:
-wget -q https://dl.winehq.org/wine-builds/winehq.key >/dev/null || die "* wget winehq.key fail!"
-sudo apt-key add <./winehq.key || die "* apt-key fail!"
-sudo add-apt-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ ${UBUNTU_DISTRO} main" >/dev/null
-sudo apt-get -q -y update >/dev/null
-sudo apt-get -q -y install procps psmisc mpg123 x264 ffmpeg xvfb xdotool x11-apps zenity winbind cabextract gawk sed winehq-staging="${WINEHQ_STAGING_VERSION}"~"${UBUNTU_DISTRO}" wine-staging="${WINEHQ_STAGING_VERSION}"~"${UBUNTU_DISTRO}" wine-staging-amd64="${WINEHQ_STAGING_VERSION}"~"${UBUNTU_DISTRO}" wine-staging-i386="${WINEHQ_STAGING_VERSION}"~"${UBUNTU_DISTRO}" >/dev/null || die "* main apt fail!"
+configure_wine_for_full_base() {
+	# add deps for wine:
+	sudo dpkg --add-architecture i386 >/dev/null
+	sudo add-apt-repository -y ppa:cybermax-dexter/sdl2-backport >/dev/null || die "* add-apt-repository fail!"
+
+	# updating wine https://wiki.winehq.org/Ubuntu:
+	wget -q https://dl.winehq.org/wine-builds/winehq.key >/dev/null || die "* wget winehq.key fail!"
+	sudo apt-key add <./winehq.key || die "* apt-key fail!"
+	sudo add-apt-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ ${UBUNTU_DISTRO} main" >/dev/null
+	sudo apt-get -q -y update >/dev/null
+}
+
+install_full_fixed() {
+	configure_wine_for_full_base
+	sudo apt-get -q -y install procps psmisc mpg123 x264 ffmpeg xvfb xdotool zenity winbind cabextract gawk sed winehq-staging="${WINEHQ_STAGING_VERSION}"~"${UBUNTU_DISTRO}" wine-staging="${WINEHQ_STAGING_VERSION}"~"${UBUNTU_DISTRO}" wine-staging-amd64="${WINEHQ_STAGING_VERSION}"~"${UBUNTU_DISTRO}" wine-staging-i386="${WINEHQ_STAGING_VERSION}"~"${UBUNTU_DISTRO}" >/dev/null || die "* main apt fail!"
+}
+
+install_full_last() {
+	configure_wine_for_full_base
+	sudo apt-get -q -y install procps psmisc mpg123 x264 ffmpeg xvfb xdotool zenity winbind cabextract gawk sed winehq-staging wine-staging wine-staging-amd64 wine-staging-i386 >/dev/null || die "* main apt fail!"
+}
+
+install_minimal_local_fixed() {
+	configure_wine_for_full_base
+	sudo apt-get -q -y install winbind cabextract winehq-staging="${WINEHQ_STAGING_VERSION}"~"${UBUNTU_DISTRO}" wine-staging="${WINEHQ_STAGING_VERSION}"~"${UBUNTU_DISTRO}" wine-staging-amd64="${WINEHQ_STAGING_VERSION}"~"${UBUNTU_DISTRO}" wine-staging-i386="${WINEHQ_STAGING_VERSION}"~"${UBUNTU_DISTRO}" || die "* main apt fail!"
+}
+
+install_minimal_local_last() {
+	configure_wine_for_full_base
+	sudo apt-get -q -y install winbind cabextract winehq-staging wine-staging wine-staging-amd64 wine-staging-i386 || die "* main apt fail!"
+}
+
+case "${1}" in
+	"1")
+		echo "* Option 1 Deps install:"
+		install_minimal_monitor
+		;;
+	"2")
+		echo "* Option 2 Deps install:"
+		install_full_fixed
+		;;
+	"3")
+		echo "* Option 3 Deps install:"
+		install_full_fixed
+		;;
+	"4")
+		echo "* Option 4 Deps install:"
+		install_minimal_monitor
+		;;
+	"5")
+		echo "* Option 5 Deps install:"
+		install_full_last
+		;;
+	"lf")
+		echo "* Option local fixed at wine-staging ${WINEHQ_STAGING_VERSION} Deps install:"
+		install_minimal_local_fixed
+		;;
+	"ll")
+		echo "* Option local last wine-staging Deps install:"
+		install_minimal_local_last
+		;;
+	*)
+		echo "No arguments parsed."
+		exit 1
+esac
