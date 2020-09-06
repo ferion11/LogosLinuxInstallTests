0147c0f64be35fbb63ae20b1f89ddda5f1d52064
DanielDevBR <dados.linux@gmail.com>
2020-09-06 09:04:28 -0300

    changed fuser to lsof, testing v2.15-rc1

--- deployscript/deploy2.sh
+++ deployscript/deploy2.sh
@@ -169,25 +169,20 @@ wait_process_using_dir() {
 	VERIFICATION_TIME=7
 	VERIFICATION_NUM=3
 
-	echo "* Starting winetricks-moded wait_process_using_dir..."
+	echo "* Starting test wait_process_using_dir..."
 	i=0 ; while true; do
 		i=$((i+1))
 		echo "-------"
-		echo "winetricks-moded: loop with i=${i}"
+		echo "test wait_process_using_dir: loop with i=${i}"
 
-		echo "winetricks-moded: sleep ${VERIFICATION_TIME}"
+		echo "test wait_process_using_dir: sleep ${VERIFICATION_TIME}"
 		sleep "${VERIFICATION_TIME}"
 
-		PID_LIST="$(fuser "${VERIFICATION_DIR}")"
-		echo "winetricks-moded PID_LIST: ${PID_LIST}"
-
-		# double quote make one bug!
-		# shellcheck disable=SC2086
-		FIST_PID="$(echo ${PID_LIST} | cut -d' ' -f1)"
-		echo "winetricks-moded FIST_PID: ${FIST_PID}"
+		FIST_PID="$(lsof -t "${VERIFICATION_DIR}" | head -n 1)"
+		echo "test wait_process_using_dir FIST_PID: ${FIST_PID}"
 		if [ -n "${FIST_PID}" ]; then
 			i=0
-			echo "winetricks-moded: tail --pid=${FIST_PID} -f /dev/null"
+			echo "test wait_process_using_dir: tail --pid=${FIST_PID} -f /dev/null"
 			tail --pid="${FIST_PID}" -f /dev/null
 			continue
 		fi
@@ -195,7 +190,7 @@ wait_process_using_dir() {
 		echo "-------"
 		[ "${i}" -lt "${VERIFICATION_NUM}" ] || break
 	done
-	echo "* End of winetricks-moded wait_process_using_dir."
+	echo "* End of test wait_process_using_dir."
 }
 
 wait_for_wine_process() {
